b65db999fe26fa06b7cfacb4fbebe6b41c4233e6
bizbuddydev <bizbuddydev@gmail.com>
2024-12-21 13:34:41 -0800

    Update social_overview.py

--- social_overview.py
+++ social_overview.py
@@ -2,7 +2,7 @@ import streamlit as st
 from google.oauth2 import service_account
 from google.cloud import bigquery
 import pandas as pd
-from datetime import date, timedelta
+from datetime import datetime, date, timedelta
 import json
 
 st.set_page_config(page_title="Social Overview", layout="wide")
