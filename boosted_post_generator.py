6bdc055709cb74e34c070665e9164f8c9892cfe8
bizbuddydev <bizbuddydev@gmail.com>
2024-12-27 16:10:35 -0800

    Update boosted_post_generator.py

--- boosted_post_generator.py
+++ boosted_post_generator.py
@@ -1,6 +1,8 @@
 from openai import OpenAI
 import streamlit as st
 
+openai_api_key = st.secrets["openai"]["api_key"]
+
 # Define links to other pages
 PAGES = {
     "Overview": "https://smp-bizbuddy-accountoverview.streamlit.app/",
@@ -22,9 +24,6 @@ for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
 
 if prompt := st.chat_input():
-    if not openai_api_key:
-        st.info("Please add your OpenAI API key to continue.")
-        st.stop()
 
     client = OpenAI(api_key=openai_api_key)
     st.session_state.messages.append({"role": "user", "content": prompt})
