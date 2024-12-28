21f63723a187eef046b04734519cdac1e2002d43
bizbuddydev <bizbuddydev@gmail.com>
2024-12-27 18:36:09 -0800

    Update boosted_post_generator.py

--- boosted_post_generator.py
+++ boosted_post_generator.py
@@ -5,10 +5,10 @@ openai_api_key = st.secrets["openai"]["api_key"]
 
 # Define links to other pages
 PAGES = {
-    "Overview 📊": "https://smp-bizbuddy-accountoverview.streamlit.app/",
-    "Posts 📱": "https://smp-bizbuddy-postoverview.streamlit.app",
-    "Scheduler 🗓️": "https://smp-bizbuddy-postscheduler.streamlit.app/",
-    "Brainstorm 💡": "https://smp-bizbuddy-v1-brainstorm.streamlit.app/"
+    "📊 Overview": "https://smp-bizbuddy-accountoverview.streamlit.app/",
+    "📱 Posts": "https://smp-bizbuddy-postoverview.streamlit.app",
+    "🗓️ Scheduler": "https://smp-bizbuddy-postscheduler.streamlit.app/",
+    "💡 Brainstorm": "https://smp-bizbuddy-v1-brainstorm.streamlit.app/"
 }
 
 # Sidebar navigation
